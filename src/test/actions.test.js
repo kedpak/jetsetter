370aed9d5b61073d59bbb6301d908d7655e20257
Kevin Pak <kedpak@yahoo.com>
2017-12-14 16:12:33 -0800

    last test update

--- src/test/actions.test.js
+++ src/test/actions.test.js
@@ -104,7 +104,7 @@ describe('Filter Actions', () => {
     it('should return the provided text', () => {
       const text = 'item name';
       const action = updateUnpackedItemsFilter(text);
-      expect(action.text).toBe(text);
+      expect(action.filter).toBe(text);
     });
   });
 
